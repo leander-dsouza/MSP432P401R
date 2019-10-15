b104f1a4721d2634068fb50a644adb6dbe858af3
Leander Stephen D'Souza <45683974+leander-dsouza@users.noreply.github.com>
2019-10-15 22:44:59 +0530

    Update Example5.2.s

--- Keil/LAB5/Example5.2.s
+++ Keil/LAB5/Example5.2.s
@@ -4,9 +4,11 @@
 				EXPORT	__main		; make __main visible to linker
 				ENTRY
 				
-__main			PROC
+__main				PROC
+				
 				MOV	R0, #10	 		;counter
-__loop			LDR	R2,=0x55555555
+__loop				
+				LDR	R2,=0x55555555
 				STR	R2, [R1]			;send it to RAM
 				ADD	R1, R1, #4		;R1 = R1 + 4 to increment pointer
 				SUBS R0, R0, #1		;R0 = R0 - 1 for dec counter 
