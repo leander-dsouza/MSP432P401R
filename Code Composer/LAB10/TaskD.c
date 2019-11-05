8c1fc1ca8dc08cdb439a7d2dca45967b54237962
Leander Stephen D'Souza <45683974+leander-dsouza@users.noreply.github.com>
2019-11-05 23:18:13 +0530

    Update TaskD.c

--- Code Composer/LAB10/TaskD.c
+++ Code Composer/LAB10/TaskD.c
@@ -2,102 +2,95 @@
 
 int main(void) {
 
-    volatile uint32_t x; //flag for seatbelt buckle
-    volatile uint32_t y; //flag for car door latch
+    volatile uint32_t x;
+    volatile uint32_t y;
 
     //left proximity sensor - P1.1
-    P1->SEL1 &= ~BIT1;
-    P1->SEL0 &= ~BIT1;
+
     P1->DIR &= ~BIT1;
     P1->REN |= BIT1;
     P1->OUT |= BIT1;
 
     //right proximity sensor - P1.4
-    P1->SEL1 &= ~BIT4;
-    P1->SEL0 &= ~BIT4;
+
     P1->DIR &= ~BIT4;
     P1->REN |= BIT4;
     P1->OUT |= BIT4;
 
+
     //forward - P1.0
-    P1->SEL1 &= ~BIT0;
-    P1->SEL0 &= ~BIT0;
+
     P1->DIR |= BIT0;
 
     //backward - P2.0
-    P2->SEL1 &= ~BIT0;
-    P2->SEL0 &= ~BIT0;
+
     P2->DIR |= BIT0;
 
     //left  - P2.1
-    P2->SEL1 &= ~BIT1;
-    P2->SEL0 &= ~BIT1;
+
     P2->DIR |= BIT1;
 
     //right  - P2.2
-    P2->SEL1 &= ~BIT2;
-    P2->SEL0 &= ~BIT2;
     P2->DIR |= BIT2;
 
 
     while (1) {
 
-        if (P1->IN & BIT1)
+        if ((P1->IN & BIT1)==BIT1)
         {
-            P1->OUT = BIT0;
             x = 0;
         }
 
-        if !(P1->IN & BIT1)
+        if (!((P1->IN & BIT1)==BIT1))
         {
-             P1->OUT &= ~BIT0;
+
              x = 1;
         }
 
 
-        if (P1->IN & BIT4)
+        if ((P1->IN & BIT4)==BIT4)
         {
-            P2->OUT = BIT0;
+
             y = 0;
         }
 
-        if !(P1->IN & BIT4)
+        if (!((P1->IN & BIT4)==BIT4))
         {
-             P2->OUT &= ~BIT0;
+
              y = 1;
         }
 
 
         if (x==0 && y==0)
         {
-            P1->OUT &= ~BIT0;
-            P2->OUT =  BIT0;
-            P2->OUT =  BIT1;
-            P2->OUT =  BIT2;
+            P1->OUT |= BIT0;
+            P2->OUT&= ~BIT0;
+            P2->OUT&= ~BIT1;
+            P2->OUT&= ~BIT2;
         }
 
         if (x==0 && y==1)
         {
-            P1->OUT =  BIT0;
-            P2->OUT =  BIT0;
-            P2->OUT&= ~BIT1;
-            P2->OUT =  BIT2;
+            P1->OUT&= ~BIT0;
+            P2->OUT&= ~BIT0;
+            P2->OUT |= BIT1;
+            P2->OUT&= ~BIT2;
         }
 
         if (x==1 && y==0)
         {
-            P1->OUT =  BIT0;
-            P2->OUT =  BIT0;
-            P2->OUT =  BIT1;
-            P2->OUT&= ~BIT2;
+            P1->OUT&= ~BIT0;
+            P2->OUT&= ~BIT0;
+            P2->OUT&= ~BIT1;
+            P2->OUT |= BIT2;
         }
 
         if (x==1 && y==1)
         {
-            P1->OUT =  BIT0;
-            P2->OUT&= ~BIT0;
-            P2->OUT =  BIT1;
-            P2->OUT =  BIT2;
+            P1->OUT&= ~BIT0;
+            P2->OUT |= BIT0;
+            P2->OUT&= ~BIT1;
+            P2->OUT&= ~BIT2;
         }
     }
 }
